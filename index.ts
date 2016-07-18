18053d9df9716e2b7d0289f2893f5d4b70a06245
Hashir Baqai <hashirbaqai@gmail.com>
2016-07-18 16:07:26 -0700

    modifying base index.ts file

--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
 import * as http from 'http';
-import * as message from './message';
 import * as log from 'loglevel';
 
 const server = http.createServer((request, response) => {
   response.statusCode = 200;
-  response.end(message.myMessage);
 });
 
 server.listen(8080, () => {
