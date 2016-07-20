0e1cebe761efe0c699e338f5049d8f8e5b575dbf
Hashir Baqai <hashirbaqai@gmail.com>
2016-07-19 20:41:37 -0700

    replacing jade with ejs and making basic dropbox api calls

--- routes/index.ts
+++ routes/index.ts
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express',
+                        dropboxToken: process.env.DROPBOX_TOKEN
+                      });
 });
 
 module.exports = router;
